c00be2ef829ab22674cc1cdda7eca7e0c1f0227e
AKINCI <ozgurakinci@outlook.com>
2023-04-02 15:09:38 +0300

    tabs config.

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 import {HomePage} from './components/home/home.page';
-import {FollowsPage} from "./components/follows/follows.page";
-import {LikesPage} from "./components/likes/likes.page";
+import {FollowsPage} from './components/follows/follows.page';
+import {LikesPage} from './components/likes/likes.page';
 
 const routes: Routes = [
   {
